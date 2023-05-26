e67786a2916f7d014af2943da058c5884806d488
Ardalber <cochezgaetan07@gmail.com>
2023-05-26 09:37:42 +0200

    mod training

--- training/main.js
+++ training/main.js
@@ -1,32 +1,36 @@
-// // CREER UN ELEMENT
+// CREER UN ELEMENT
 
-// let nouvelElement = document.createElement("div");
+let nouvelElement = document.createElement("div");
 
-// // DONNER UN ID A UN ELEMENT
+// DONNER UN ID A UN ELEMENT
 
-// nouvelElement.id = "monElement";
-
-// // AJOUTER DU CONTENU A UN ELEMENT
-
-// nouvelElement.textContent = "Contenu de mon élément";
-
-
-// // AJOUT DE L'ELEMENT AU PARENT
-
-// let parentElement = document.getElementById("container");
-
-// parentElement.appendChild(nouvelElement);
-
-// Création de l'élément
-
-// Création de l'élément
-var nouvelElement = document.createElement("div");
 nouvelElement.id = "monElement";
+
+// AJOUTER DU CONTENU A UN ELEMENT
+
 nouvelElement.textContent = "Contenu de mon élément";
 
-// Ajout de l'élément au parent
-var parentElement = document.getElementById("container");
+
+// AJOUT DE L'ELEMENT AU PARENT
+
+    //CREATION DU PARENT
+
+let parentElement = document.getElementById("container");
+
+// AJOUT DE L'ELEMENT AU PARENT
+
 parentElement.appendChild(nouvelElement);
 
-console.log(nouvelElement);
+
+// SELECTION DE L'ELEMENT A STYLISER
+
+var element = document.getElementById('monElement');
+
+// STYLISER L'ELEMENT 
+
+element.style.backgroundColor = 'green';
+element.style.color = 'white';
+element.style.fontSize = '20px';
+
+
 
