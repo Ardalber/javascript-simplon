ddbf3351c63951d7b58f5c58d4cdcace5737ff76
Ardalber <cochezgaetan07@gmail.com>
2023-05-27 13:54:53 +0200

    training array

---
+++ array/main.js
@@ -0,0 +1,36 @@
+// creer un tableau
+
+let array = ['salut','les','copains']
+console.log(array);
+
+// // retirer le dernier element du tableau
+
+// array.pop ()
+// console.log(array);
+
+// // ajouter un element à la fin du tableau
+
+// array.push('yéyé')
+// console.log(array);
+
+// // ajouter un element au début du tableau
+
+// array.unshift('hé');
+// console.log(array);
+
+// // supprimer le premier element du tableau
+
+// array.shift();
+// console.log(array);
+
+// // trouver l'index d'un élément dans un tableau
+
+// var searchIndex = array.indexOf('yéyé');
+// console.log(searchIndex);
+
+// // supprimer un élément grâce à son index
+
+array.splice(1,2,'coucou');
+console.log(array);
+
+
