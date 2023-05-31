bfe36da0c09072254af870c73ff4a5d7419adf16
Ardalber <cochezgaetan07@gmail.com>
2023-05-31 13:05:20 +0200

    training

---
+++ projets-perso/dom.js
@@ -0,0 +1,5 @@
+"use strict";
+
+import box from "./main.js";
+
+alert(box);
---
+++ projets-perso/main.js
@@ -0,0 +1,7 @@
+export { default } from "/home/oem/documents/javascript-simplon/projets-perso/main.js";
+
+("use strict");
+
+let carambar = 13;
+
+console.log(carambar);
