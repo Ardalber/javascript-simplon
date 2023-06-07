0d9216e70c915f7eaeb16cbf00d01d55d508f54f
Ardalber <cochezgaetan07@gmail.com>
2023-06-07 20:25:59 +0200

    adding files

--- kata-js/11-keep-hydrated.js
+++ kata-js/11-keep-hydrated.js
@@ -1,6 +1,5 @@
 function litres(time) {
-    // pour chaque time , litters doit s'incrémenter de 0,5l 
-   let litters = Math.floor(time * 0.5)
-   return litters;
-  };
-  
+  // pour chaque time , litters doit s'incrémenter de 0,5l
+  let litters = Math.floor(time * 0.5);
+  return litters;
+}
