5e4dee20e0b38269d5e9ee67d897597089306304
Ardalber <cochezgaetan07@gmail.com>
2023-06-18 15:25:03 +0200

    trainning

--- training/array-training/main.js
+++ training/array-training/main.js
@@ -1,60 +1,7 @@
-// // creer un tableau
+'use strict'
+let x = ['5', '0', 9, 3, 2, 1, '9', 6, 7];
 
-let array = ["salut", "les", "copains"];
-
-console.log("1");
-console.log(array);
-// // // retirer le dernier element du tableau
-
-array.pop();
-console.log("2");
-console.log(array);
-
-// // // ajouter un element à la fin du tableau
-
-array.push("yéyés");
-console.log("3");
-console.log(array);
-
-// // // ajouter un element au début du tableau
-
-array.unshift("hé");
-console.log("4");
-console.log(array);
-
-// // // supprimer le premier element du tableau
-
-array.shift();
-console.log("5");
-console.log(array);
-
-//  trouver l'index d'un élément dans un tableau
-
-var searchIndex = array.indexOf("yéyé");
-console.log(searchIndex);
-console.log(array);
-// // // supprimer un élément grâce à son index array.
-
-// splice(start, deleteCount, item1, item2);
-
-// - start : début de modification
-// - deleteCount : étendue de la modification
-// - item , nom  des éléments facultatifs à ajouter au tableau
-
-// // exemple :
-
-// // retire "salut"
-
-array.splice(0, 1);
-console.log("6");
-console.log(array);
-
-array.unshift("salut", "comment", "vont");
-console.log("7");
-console.log(array);
-
-// enlever 'comment' et 'vont'
-
-array.splice(1, 2);
-console.log("8");
-console.log(array);
+function sumMix(x){
+    return x.reduce((sum,current) => sum + current, 0);
+}
+console.log(sumMix(x));
